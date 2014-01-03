a80db80d257c1362a841ed8fd85f91573190fb20
misa <mengting.cai@core.elong.com>
2014-01-03 16:37:38 +0800

    2014.1.3

--- src/misa.js
+++ src/misa.js
@@ -1,17 +1,43 @@
 var Misa = (function(window) {
-	var m = {},key;
+	var m = {},key,
+	tagRE = /^<(\w+)>$/;		// test tag
+	
+	// create tag
+	m.tag = function(name, property) {
+		var dom = $(document.createElement(name));
+		for(key in property) {
+			dom.attr(key, property[key]);
+		}
+		return dom;
+	}
 
-	m.init = function(selector) {
+	// change nodeList to Misa Collection
+	m.M = function(dom, nodeList) {
+		if(!nodeList.length) {
+			dom[0] = nodeList;
+			return dom;
+		}
+		dom.length = nodeList.length;
+    	for(key = nodeList.length - 1; key >= 0; key--) {
+    		dom[key] = nodeList[key]
+    	}
+    	return dom
+	}
+	
+	m.init = function(selector, property) {
 		var dom = [];
 		dom.__proto__ = $.fn;
 		if(typeof selector == 'string') {
+			if(selector[0] == '<' && tagRE.test(selector)) {
+				dom = m.tag(RegExp.$1, property);
+				return dom
+			} 
 			var nodeList = document.querySelectorAll(selector);
-	    	dom.length = nodeList.length;
-	    	for(var i = nodeList.length - 1; i >= 0; i--) {
-	    		dom[i] = nodeList[i]
-	    	}
+    		return m.M(dom, nodeList)
 		}
-		return dom
+		if(typeof selector == 'object') {
+			return m.M(dom, selector)
+		}
 	}
 	
 	/*
@@ -21,9 +47,10 @@ var Misa = (function(window) {
 	 * $()	//=> empty Array 
 	 * $('div')  //=> all DIV elements on the page
 	 * $('#content') //=> element with ID "content"
+	 * $('<div>', {css: 'tag'}) //=> create <div> with class 'tag'
 	 */
-	var $ = function(selector) {
-		return m.init(selector)
+	var $ = function(selector, property) {
+		return m.init(selector, property)
 	}
 	//  prototype for $() collection
 	$.fn = {
@@ -84,11 +111,46 @@ var Misa = (function(window) {
 				})
 			}
 		}
+		/*
+		 * attribute operation
+		 * $('p span').attr('class', 'highlight')	//set single attribute
+		 */
+		,attr: function(name, value) {
+			this.each(function() {
+				this.setAttribute(name, value);
+			})
+			return this;
+		}
+		/*
+		 * set innerHTML
+		 * $('#year').html('<div class="text">2014</div>')
+		 */
+		,html: function(html) {
+			this.each(function() {
+				this.innerHTML = html;
+			})
+			return this;
+		}
+		// get the first child of each element of collection
+		,first: function() {
+			var dom = [];
+			this.each(function() {
+				dom.push(this.childNodes[0])
+			})
+			dom.__proto__ = $.fn;
+			return dom;
+		}
+		// remove node self
+		,remove: function() {
+			this.each(function() {
+				this.parentNode.removeChild(this);
+			})
+		}
 	}
 	/*
 	 * insert DOM
 	 * append(content) ⇒  self
-	 * the content can be an HTML string or a DOM node
+	 * the content can be an HTML string, a DOM node or collection
 	 * for example:
 	 * $('ul').append('<li>item</li>')	//insert <li>item</li> immediately before each </ul>
 	 * $('p').before('<br/>')  //insert <br> immediately before each <p>
@@ -102,6 +164,15 @@ var Misa = (function(window) {
 	for (key in adjacencyOperators) {
 		$.fn[key] = (function(operator) {
 			return function(html){
+				//insert collection
+				if(typeof html == 'object') {
+					var _ = this;
+					return html.each(function(node) {
+						_.each(function(el) {
+							el.insertAdjacentElement(operator, node)
+						})
+					})
+				}
 				return this.each(function(el) {
 					el['insertAdjacent' + (html instanceof Element ? 'Element' : 'HTML')](operator, html)
 				})
